74346e9864bbf8907912b6057d3d641945722a81
JFunescr <jfunes70944@ufide.ac.cr>
2025-11-24 14:09:08 -0600

    first commit

---
+++ ProyectoGrupo7/src/proyectogrupo7/ProyectoGrupo7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectogrupo7;
+
+/**
+ *
+ * @author jafet
+ */
+public class ProyectoGrupo7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
